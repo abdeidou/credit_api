4127863afeb02c5ade26f47ed5c2b5408d91c68d
ABDELAAZIZ IDOUMOHMED <aziz@MacBook-Air-de-ABDELAAZIZ.local>
2024-02-23 06:08:39 +0100

    commit 1

---
+++ main.py
@@ -0,0 +1,65 @@
+# import packages
+import pandas as pd
+import streamlit as st
+import requests
+import subprocess
+
+subprocess.run(["python", "model_data.py"])
+
+# session state
+if 'predict' not in st.session_state:
+    st.session_state['predict'] = False
+if 'customer_found' not in st.session_state:
+    st.session_state['customer_found'] = False
+if 'customer_data' not in st.session_state:
+    st.session_state['customer_data'] = []
+if 'customer_id' not in st.session_state:
+    st.session_state['customer_id'] = -1
+
+# buttons methods
+def predict_button():
+    st.session_state['predict'] = True
+
+def search_button():
+    st.session_state['predict'] = False
+    st.session_state['customer_found'] = False
+    st.session_state['customer_id'] = -1
+    st.session_state['customer_data'] = []
+
+# sidebar code
+st.sidebar.header('Informations client')
+first_name = st.sidebar.text_input("Nom", '')
+last_name = st.sidebar.text_input("Prénom", '')
+customer_id = st.sidebar.text_input("Identifiant*", '')
+if st.sidebar.button('Chercher', on_click=search_button):
+    st.session_state['customer_id'] = customer_id
+    response = requests.get("http://localhost:1111/customer_data", params={"customer_id": customer_id}).json()
+    st.session_state['customer_data'] = pd.read_json(response['customer_data'], dtype={'SK_ID_CURR': str})
+    if st.session_state['customer_data'].empty:
+        st.sidebar.write(":red[Client non trouvé]")
+    else:
+        st.session_state['customer_found'] = True
+
+# app code
+if st.session_state['customer_found']:
+    st.write("""
+    # Prédiction de remboursement
+    """)
+    st.subheader('Données client')
+    st.write(st.session_state['customer_data'])
+    st.button('Prédire', on_click=predict_button)
+    if st.session_state['predict']:
+        response = requests.get("http://localhost:1111/predict", params={"customer_id": customer_id}).json()
+        st.session_state['customer_predict'] = response['customer_predict']
+        predict = {'Bon client': [str(round(100 * st.session_state['customer_predict'][0][0], 1)) + '%'],
+                   'Mauvais client': [str(round(100 * st.session_state['customer_predict'][0][1], 1)) + '%']}
+        df_predict = pd.DataFrame(predict)
+        st.dataframe(df_predict, hide_index=True)
+else:
+    st.image('./logo.png')
+    st.write("Application de scoring crédit pour calculer la probabilité qu’un client rembourse son\
+             crédit à la consommation pour des personnes ayant peu ou pas du tout d'historique de prêt.")
+
+
+
+# streamlit run main.py
---
+++ model_data.py
@@ -0,0 +1,40 @@
+import json
+from flask import Flask, request
+import pandas as pd
+import pickle
+
+
+app = Flask(__name__)
+
+# read csv file
+data_test = pd.read_csv("./application_test.csv")
+data_test_ohe = pd.read_csv("./application_test_ohe.csv")
+customers_data = data_test
+customers_data_ohe = data_test_ohe
+
+# load model
+lgbm = pickle.load(open('./best_model.pickle', 'rb'))
+
+
+@app.route('/customer_data', methods=['GET'])
+def customer_data():
+    customer_id = request.args.get("customer_id")
+    customer_row = customers_data[customers_data['SK_ID_CURR']==int(customer_id)]
+    customer_index = customer_row.index
+    response = {'customer_data': customer_row.to_json()}
+    return json.dumps(response)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    customer_id = request.args.get("customer_id")
+    customer_row = customers_data[customers_data['SK_ID_CURR'] == int(customer_id)]
+    customer_index = customer_row.index
+    customer_row_ohe = customers_data_ohe.iloc[customer_index].drop(columns=['SK_ID_CURR'], axis=1)
+    lgbm.predict_proba(customer_row_ohe)
+    response = {'customer_predict': lgbm.predict_proba(customer_row_ohe).tolist()}
+    return json.dumps(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1111, debug=True)
+
+#python flask.py
