b2a90e6c872c6e24ddb67e968abfaa6f08be1a60
ABDELAAZIZ IDOUMOHMED <aziz@MacBook-Air-de-ABDELAAZIZ.local>
2024-03-01 12:37:04 +0100

    modif test file name

---
+++ tests/test_api.py
@@ -0,0 +1,46 @@
+import pytest
+import json
+import requests
+from sources.model_data import customers_data, customers_data_ohe, lgbm
+
+@pytest.fixture(scope="module")
+def api_url():
+    return "http://localhost:1111"
+
+@pytest.fixture(scope="module")
+def customer_id():
+    return 100028
+
+@pytest.fixture(scope="module")
+def expected_customer_data(customer_id):
+    return customers_data[customers_data['SK_ID_CURR'] == customer_id].to_json()
+
+@pytest.fixture(scope="module")
+def expected_customer_predict(customer_id):
+    customer_row = customers_data[customers_data['SK_ID_CURR'] == customer_id]
+    customer_row_ohe = customers_data_ohe.iloc[customer_row.index].drop(columns=['SK_ID_CURR'], axis=1)
+    return lgbm.predict_proba(customer_row_ohe).tolist()
+
+def test_customer_data_api(api_url, customer_id, expected_customer_data):
+    """Test the /customer_data API endpoint."""
+    # Make request to API endpoint
+    with requests.get(f"{api_url}/customer_data", params={"customer_id": customer_id}) as response:
+        # Assert response status code
+        assert response.status_code == 200
+
+        # Assert response content
+        response_data = json.loads(response.text)
+        assert response_data['customer_data'] == expected_customer_data
+
+def test_predict_api(api_url, customer_id, expected_customer_predict):
+    """Test the /predict API endpoint."""
+    # Make request to API endpoint
+    with requests.get(f"{api_url}/predict", params={"customer_id": customer_id}) as response:
+        # Assert response status code
+        assert response.status_code == 200
+
+        # Assert response content
+        response_data = json.loads(response.text)
+        assert response_data['customer_predict'] == expected_customer_predict
+
+
