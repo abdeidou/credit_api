068289f6b400bd55fe5898e44d7140e13069377a
ABDELAAZIZ IDOUMOHMED <aziz@MacBook-Air-de-ABDELAAZIZ.local>
2024-03-17 13:40:21 +0100

    correct flask

--- sources/model_data.py
+++ sources/model_data.py
@@ -13,8 +13,8 @@ def load_model(file_path):
     return model
 
 # Read CSV files and load model
-data_test = pd.read_csv("./data/application_test.csv")
-data_test_ohe = pd.read_csv("./data/application_test_ohe.csv")
+data_test = pd.read_csv("./data/application_test.csv", dtype={'SK_ID_CURR': str})
+data_test_ohe = pd.read_csv("./data/application_test_ohe.csv", dtype={'SK_ID_CURR': str})
 customers_data = data_test
 customers_data_ohe = data_test_ohe
 model_path = "./data/best_model.pickle"
