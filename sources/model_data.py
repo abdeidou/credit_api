7ab6c83407aaa2c7e9b0bdad7071d48a7b7e341f
ABDELAAZIZ IDOUMOHMED <aziz@MacBook-Air-de-ABDELAAZIZ.local>
2024-03-17 13:55:21 +0100

    correct flask

--- sources/model_data.py
+++ sources/model_data.py
@@ -42,8 +42,8 @@ def predict():
 
 if __name__ == '__main__':
     #app.run(host="0.0.0.0", port=6060, debug=False)
-    #serve(app, host="0.0.0.0", port=5000)
-    serve(app, host="localhost", port=5000)
+    serve(app, host="0.0.0.0", port=5000)
+    #serve(app, host="localhost", port=5000)
 
 #if __name__ == '__main__':
 #    app.run(host="localhost", port=8080, debug=True)
