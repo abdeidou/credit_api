ed6b40745d4254c8513787d309a6801643dc124a
ABDELAAZIZ IDOUMOHMED <aziz@MacBook-Air-de-ABDELAAZIZ.local>
2024-03-17 10:29:17 +0100

    correct code predict

--- sources/model_data.py
+++ sources/model_data.py
@@ -30,19 +30,12 @@ def customer_data():
 @app.route('/predict', methods=['GET'])
 def predict():
     customer_id = request.args.get("customer_id")
-    if customer_id is None:
-        return json.dumps({"error": "Customer ID is missing"}), 400
-    try:
-        customer_id = int(customer_id)
-        customer_row = customers_data[customers_data['SK_ID_CURR'] == customer_id]
-        if customer_row.empty:
-            return json.dumps({"error": "Customer not found"}), 404
+    customer_row = customers_data[customers_data['SK_ID_CURR'] == customer_id]
+    if not customer_row.empty:
         customer_row_ohe = customers_data_ohe.iloc[customer_row.index].drop(columns=['SK_ID_CURR'], axis=1)
         predictions = lgbm.predict_proba(customer_row_ohe).tolist()
         response = {'customer_predict': predictions}
         return json.dumps(response)
-    except ValueError:
-        return json.dumps({"error": "Invalid customer ID"}), 400
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
